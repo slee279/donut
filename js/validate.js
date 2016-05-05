64347e7b20515617e07c4775a083ed950bb9e5c5
Suk <slee279@illinois.edu>
2016-05-05 15:59:12 -0500

    test

--- js/validate.js
+++ js/validate.js
@@ -24,7 +24,7 @@ $(document).ready(function()
                         required: true
                     },
                 }
-            form.submit();
+            form.submit()
             }
         })
         
